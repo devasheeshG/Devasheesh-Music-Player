2b77763f76d02816dcf8f7717d86d11cd187c448
devasheeshG <72092910+devasheeshG@users.noreply.github.com>
2020-10-28 18:59:46 +0530

    added code files

---
+++ files/Devasheesh Music Player/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        AxWindowsMediaPlayer1.URL = OpenFileDialog1.FileName
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+
+    End Sub
+
+    Private Sub TrackBar1_Scroll(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TrackBar1.Scroll
+        AxWindowsMediaPlayer1.settings.volume = TrackBar1.Value
+
+    End Sub
+
+
+End Class
